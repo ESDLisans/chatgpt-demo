53533e6de9cf22eb299335610a26dc4793abbf96
ESDLisans <122188449+ESDLisans@users.noreply.github.com>
2023-04-02 02:10:45 +0300

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,13 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly OPENAI_API_KEY: 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
+  readonly OPENAI_API_KEY: "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"
   readonly HTTPS_PROXY: string
-  readonly OPENAI_API_BASE_URL: string
+  readonly OPENAI_API_BASE_URL: "https://api.openai.com"
   readonly HEAD_SCRIPTS: string
   readonly SECRET_KEY: string
   readonly SITE_PASSWORD: string
-  readonly OPENAI_API_MODEL: string
+  readonly OPENAI_API_MODEL: "gpt-3.5-turbo"
 }
 
 interface ImportMeta {
