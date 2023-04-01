6fc32c598e5af9c524e2325a9cf3acd630dc7166
ESDLisans <122188449+ESDLisans@users.noreply.github.com>
2023-04-02 02:20:45 +0300

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly OPENAI_API_KEY: "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"
+  readonly OPENAI_API_KEY: "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"
   readonly HTTPS_PROXY: "http://127.0.0.1:7890"
   readonly OPENAI_API_BASE_URL: "https://api.openai.com"
   readonly HEAD_SCRIPTS: string
